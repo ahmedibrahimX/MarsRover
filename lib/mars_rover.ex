5d991b461e208ecd37a655ec8198e5c8ec484022
ahmed-ibrahim-99 <a.ibrahim.abdellatif@gmail.com>
2024-01-13 20:25:49 +0200

    Rover - get status

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,18 +1,22 @@
 defmodule MarsRover do
-  @moduledoc """
-  Documentation for `MarsRover`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  defstruct [x: 0, y: 0, direction: :N, name: nil]
 
-  ## Examples
+  def start_link({x, y, direction, name}) do
+    GenServer.start_link(__MODULE__, {x, y, direction, name}, name: String.to_atom(name))
+  end
 
-      iex> MarsRover.hello()
-      :world
+  def init({x, y, direction, name}) do
+    {:ok, %MarsRover{x: x, y: y, direction: direction, name: name}}
+  end
 
-  """
-  def hello do
-    :world
+  def get_state(name) do
+    GenServer.call(String.to_atom(name), :get_state)
+  end
+
+  def handle_call(:get_state, _from, state) do
+    %MarsRover{x: x, y: y, direction: d} = state
+    {:reply, {:ok, {x, y, d}}, state}
   end
 end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -2,7 +2,9 @@ defmodule MarsRoverTest do
   use ExUnit.Case
   doctest MarsRover
 
-  test "greets the world" do
-    assert MarsRover.hello() == :world
+  test "get_state should return current state" do
+    {:ok, _} = MarsRover.start_link({9, 9, :N, "rover0"})
+    {:ok, state} = MarsRover.get_state("rover0")
+    assert state == {9, 9, :N}
   end
 end

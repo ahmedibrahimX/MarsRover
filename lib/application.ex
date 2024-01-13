c2faad7c3c7d48a710d9039db5a4734501de8b45
ahmed-ibrahim-99 <a.ibrahim.abdellatif@gmail.com>
2024-01-13 23:47:12 +0200

    Add minimal web module implementation

--- lib/application.ex
+++ lib/application.ex
@@ -4,7 +4,8 @@ defmodule MarsRover.Application do
   def start(_type, _args) do
     children = [
       Supervisor.child_spec({Registry, [keys: :unique, name: MarsRover.Registry]}, id: :rover_registry),
-      Supervisor.child_spec({RoverSupervisor, []}, id: RoverSupervisor)
+      Supervisor.child_spec({RoverSupervisor, []}, id: RoverSupervisor),
+    Plug.Adapters.Cowboy.child_spec(:http, MarsRover.Web.Router, [], port: 3000)
     ]
     opts = [strategy: :one_for_one, name: Application.Supervisor]
     Supervisor.start_link(children, opts)
---
+++ lib/web/router.ex
@@ -0,0 +1,13 @@
+defmodule MarsRover.Web.Router do
+  use Plug.Router
+
+  plug(Plug.Parsers, parsers: [:json], json_decoder: Poison)
+  plug(:match)
+  plug(:dispatch)
+
+  get "/ping", do: send_resp(conn, 200, Poison.encode!(%{message: "pong"}))
+
+  match(_) do
+    send_resp(conn, 404, "")
+  end
+end
